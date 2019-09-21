6e0fac50ddc81ece52eb20bcbe5aea3a7e2f05b4
NinaA <averill.nina@gmail.com>
2019-09-22 00:03:02 +1000

    Refactored code base and directories to ensure PEP8 compliance

---
+++ pathfinding/greedy_search.py
@@ -0,0 +1,72 @@
+from math import sqrt, inf
+from itertools import product
+import matplotlib.pyplot as plt
+import heapq
+import numpy as np
+import random
+from utils.priority_queue import PriorityQueue
+from heuristic import manhattan_cost
+
+# A* Search
+
+# Greedy Search
+def _is_obstacle(pos, grid, threshold=0.75):
+    return grid_cost(pos, grid) > threshold
+
+def _grid_cost(pos, grid):
+    return grid[pos[0]][pos[1]]
+
+def _is_goal(pos, goal):
+    return pos == goal
+
+def _reconstruct_path(end, prev):
+    result = [end]
+
+    curr = end 
+    while curr in prev.keys():
+        curr = prev[curr]
+        result.insert(0, curr)
+    return result
+
+def _get_neighbours(pos, grid):
+    n_cols = len(grid[0])
+    n_rows = len(grid)
+
+    neighbours = []
+
+    for i in [-1, 0, + 1]:
+        for j in [-1, 0, +1]:
+            x = pos[0] + i
+            y = pos[1] + j 
+            if (0 <= x < n_rows) and (0 <= y < n_cols) and (x, y) != pos: 
+                neighbours.append((x, y))
+    return neighbours
+
+def greedy_search(grid, start, end, heuristic_cost=manhattan_cost):
+    closed_set = set()
+    open_set = PriorityQueue()
+
+    open_set.put(start, (heuristic_cost(start, end), grid_cost(start, grid)))
+
+    prev = {}
+
+    while not open_set.empty():
+        curr = open_set.get()
+
+        if is_goal(curr, end):
+            return _reconstruct_path(curr, prev)
+
+        for neighbour in _get_neighbours(curr, grid):
+            if neighbour in closed_set:
+                continue
+
+            elif _is_obstacle(neighbour, grid):
+                continue
+
+            if neighbour not in open_set:
+                open_set.put(neighbour, (heuristic_cost(neighbour, end), grid_cost(neighbour, grid)))
+
+            prev[neighbour] = curr
+        closed_set.add(curr)
+
+    return []
---
+++ pathfinding/test_pathfinding.py
@@ -0,0 +1,51 @@
+from a_star import a_star_search
+from dijkstra import dijkstra_search
+from greedy_search import greedy_search
+
+def basic_grid():
+    array = [0.0, 0.1, 0.6, 0.8]
+    grid = np.random.choice(array, (8, 8))
+    return grid
+
+def norm_grid(size=(15, 15)):
+    grid = []
+    n_rows, n_cols = size
+    for i in range(n_rows):
+        grid.append([])
+
+        for j in range(n_cols):
+            val = np.random.normal(loc=0.2, scale = 0.7)
+            while val < 0 or val > 1:
+                val = np.random.normal(loc=0.5, scale=0.7)
+
+            grid[i].append(val)
+
+    return grid
+
+def plot_grid(grid, path):
+    plt.imshow(grid, cmap='hot', interpolation='nearest')
+
+    x_pos = []
+    y_pos = []
+
+    for x in path:
+        x_pos.append(x[1])
+        y_pos.append(x[0])
+    plt.plot(x_pos,y_pos, "ro",color = "Green")
+
+    plt.show()
+    
+def test_search(grid, search):
+    start =  (1, 1)
+    goal = (len(grid) - 1, len(grid[0]) - 1)
+    path = search(grid, start, goal)
+    print(path)
+    plot_grid(grid, path)
+
+test_grid = norm_grid()
+
+print("ASTAR")
+test_search(test_grid, a_star_search)
+print("Greedy")
+test_search(test_grid, greedy_search)
+test_search(test_grid, dijkstra_search)
