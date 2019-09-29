cfe876b8b03d972118b4a4301569fad0f54255cc
Jack McRobbie <Odyssey@Troys-MacBook-Air.local>
2019-09-29 13:26:00 +1000

    Pushing changes for collaborators

---
+++ nova_rover_demos/pointCloudProcessing/cloud.py
@@ -0,0 +1,13 @@
+'''
+cloud.py 
+Implements class cloud - which is a n dimensional point ploud stored in a numpy array
+Author: Jack McRobbie
+'''
+
+class cloud:
+	def __init__(self,dims,N,cloud = []):
+		self.count = N 
+		self.dimension = dims
+		if type(cloud) == "list"
+		self.cloudData = np.array([])
+	def 
--- nova_rover_demos/pointCloudProcessing/ptcloudgen.py
+++ nova_rover_demos/pointCloudProcessing/ptcloudgen.py
@@ -7,17 +7,26 @@ processable by processes.
 
 Author: Jack McRobbie
 '''
+import random as rn
 
 class point_cloud_gen:
+	self.clouds = []
 	self.dims = NaN
-	def __init__ (self,size, features, noise = 1,):
+	def __init__ (self,size, features,noise = 1,N = 1000):
 		'''
 		size: the bounds on the range of the point cloud values. Is a list of n tuples where n is the number of dimensions
 		eg. [(-1,1),(-2,5)]
 		'''
 		self.dims = len(size)
-		if self.dims = 0 or self.dims > 3:
+		if self.dims == 0 or self.dims > 3:
 			raise Exception("Input length of size should not be 0 or greater than 3. The vale was: {}" format(self.dim))
 		self.var = 1
 		self.num_features = features
-	def 
+		self.num_points = N
+	def _gen_white_noise(self):
+	''' 
+	Generates an output cloud of points with a uniform random distribution.
+	No specific feautures.
+	'''
+	cloud = []
+	for i in range(self.num_points):
