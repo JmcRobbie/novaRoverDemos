41e01913277b1f207e3bd104fc33807c78327f8d
Jack McRobbie <jackmcrobbie@hotmail.com>
2019-09-30 18:46:54 +1000

    stashing changes

--- nova_rover_demos/pointCloudProcessing/cloud.py
+++ nova_rover_demos/pointCloudProcessing/cloud.py
@@ -3,11 +3,14 @@ cloud.py
 Implements class cloud - which is a n dimensional point ploud stored in a numpy array
 Author: Jack McRobbie
 '''
-
+import numpy as np
 class cloud:
 	def __init__(self,dims,N,cloud = []):
 		self.count = N 
 		self.dimension = dims
-		if type(cloud) == "list"
-		self.cloudData = np.array([])
-	def 
+		if type(cloud) == type([]):
+			self.cloudData = cloud
+		elif type(cloud) == type(np.array()):
+			self.cloudData = np.array([])
+	def setCloud(self,data):
+		self.cloudData = data
--- nova_rover_demos/pointCloudProcessing/ptcloudgen.py
+++ nova_rover_demos/pointCloudProcessing/ptcloudgen.py
@@ -7,26 +7,67 @@ processable by processes.
 
 Author: Jack McRobbie
 '''
+import numpy as np
 import random as rn
+from cloud import cloud
+from matplotlib import pyplot
+from mpl_toolkits.mplot3d import Axes3D
 
 class point_cloud_gen:
-	self.clouds = []
-	self.dims = NaN
+
 	def __init__ (self,size, features,noise = 1,N = 1000):
 		'''
 		size: the bounds on the range of the point cloud values. Is a list of n tuples where n is the number of dimensions
 		eg. [(-1,1),(-2,5)]
 		'''
+
 		self.dims = len(size)
 		if self.dims == 0 or self.dims > 3:
-			raise Exception("Input length of size should not be 0 or greater than 3. The vale was: {}" format(self.dim))
+			raise Exception("Input length of size should not be 0 or greater than 3")
 		self.var = 1
 		self.num_features = features
 		self.num_points = N
+		self.size = size
 	def _gen_white_noise(self):
-	''' 
-	Generates an output cloud of points with a uniform random distribution.
-	No specific feautures.
-	'''
-	cloud = []
-	for i in range(self.num_points):
+		''' 
+		Generates an output cloud of points with a uniform random distribution.
+		No specific feautures.
+		'''
+		if self.num_features is not 0:
+			raise Exception("Uniform cloud cannot have more than 0 features")
+		dat = np.zeros([self.num_points,self.dims])
+		for i in range(self.num_points):
+			for j in range(self.dims):
+				dat[i][j] = rn.uniform(self.size[j][0],self.size[j][1])
+		self.ptcloud = dat
+		return
+	def uniformCloud(self):
+		'''
+		Public implementation of white noise generator
+		'''
+		self.num_features = 0 ## Encorced by cloud type.
+		self._gen_white_noise()
+		return 
+
+	def plotCloud(self):
+		fig = pyplot.figure()
+		ax = Axes3D(fig)
+		x = self.ptcloud[:,0]
+		y = self.ptcloud[:,1]
+		z = self.ptcloud[:,2]
+		ax.scatter(x,y,z)
+		pyplot.show()
+
+
+
+def test_ptcloud():
+	size = [(0,1),(0,2),(0,5)]
+	cld = point_cloud_gen(size,0)
+	cld.uniformCloud()
+	cld.plotCloud()
+
+
+
+
+if __name__ == '__main__':
+    test_ptcloud()
