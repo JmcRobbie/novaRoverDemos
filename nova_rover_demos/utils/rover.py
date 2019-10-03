1d71f04688103b3300b3ab94f1dd8cbcc84a5382
NinaA <averill.nina@gmail.com>
2019-10-03 10:58:00 +1000

    Added Pose class and added waypoint logic to Rover

--- nova_rover_demos/utils/rover.py
+++ nova_rover_demos/utils/rover.py
@@ -1,9 +1,7 @@
-class Rover:
+class Rover(Pose):
     def __init__(self,x,y,th):
         # Initial conditions selected arbitrarily
-        self.x = x
-        self.y = y
-        self.theta = th
+        super(x, y, th)
         self.v = 0
         self.world = []
 
@@ -13,15 +11,41 @@ class Rover:
         self.theta = state[2]
         self.v = state[3]
 
-    def update_map(self,map):
+    def update_map(self, map):
         self.world = map
-    def check_error(self,rover2):
+        
+    def check_error(self, pose):
         '''
-        Returns the error between the self object an another rover, this is used for checking exit 
+        Returns the error between the self object and another rover, this is used for checking exit 
         conditions on pose control loops.
         '''
-        dx = abs(self.x - rover2.x)
-        dy = abs(self.y - rover2.y)
-        dth = abs(self.theta - rover2.theta)
+        dx = abs(self.x - pose.x)
+        dy = abs(self.y - pose.y)
+        dth = abs(self.theta - pose.theta)
         
         return dx*dx+dy*dy+dth*dth
+    
+    
+    def load_waypoints(self, waypoints):
+        self._waypoints = list(waypoints)
+        self._target = self.waypoints.pop(0)
+    
+    def update_waypoints(self):
+        if self._waypoints:
+            self._target = self.waypoints.pop(0)
+            return self._target
+        
+    def get_target_waypoint(self):
+        return self._target
+    
+    def set_target_range(self, t_range):
+        self.range = t_range
+    
+    def goal_reached(self, )
+        
+class Pose:
+    def __init__(self, x, y, theta):
+        self.x = x
+        self.y = y
+        self.theta = theta
+    
