099b5f2fc2e047c5f584c7e055730ce035e6fa3b
verbal-noun <thats.kaif@gmail.com>
2020-01-23 01:56:48 +1100

    Declaring the slam covariance and sim parameters

--- nova_rover_demos/SLAM/fastSLAM.py
+++ nova_rover_demos/SLAM/fastSLAM.py
@@ -1,5 +1,6 @@
 '''
 Implementation of the fast slam algorithm 
+For a step by step implementation please check the Jupyter Notebook
 NOTE: 
 
 We do not own the entire code 
@@ -10,3 +11,12 @@ import math
 
 import matplotlib.pyplot as plt
 import numpy as np
+
+# Fast SLAM covariance
+Q = np.diag([3.0, np.deg2rad(10.0)]) ** 2
+R = np.diag([1.0, np.deg2rad(20.0)]) ** 2
+
+#  Simulation parameter
+Qsim = np.diag([0.3, np.deg2rad(2.0)]) ** 2
+Rsim = np.diag([0.5, np.deg2rad(10.0)]) ** 2
+OFFSET_YAWRATE_NOISE = 0.01
