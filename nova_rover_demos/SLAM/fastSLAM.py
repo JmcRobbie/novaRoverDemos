01da92419e450c18fbe7ba95bce561514fc37607
verbal-noun <thats.kaif@gmail.com>
2020-01-23 01:55:18 +1100

    Importing necessary libraries

---
+++ nova_rover_demos/SLAM/fastSLAM.py
@@ -0,0 +1,12 @@
+'''
+Implementation of the fast slam algorithm 
+NOTE: 
+
+We do not own the entire code 
+The code is partly taken from the Python Robotics Repo 
+'''
+
+import math
+
+import matplotlib.pyplot as plt
+import numpy as np
